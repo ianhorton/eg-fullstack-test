81f608d37aed601f65a5bc734265e9291b139828
irh-chktrd <ian.horton@checkatrade.com>
2024-10-23 15:02:41 +0200

    add jwt strategy and update access components

--- packages/backend/src/access/access.module.ts
+++ packages/backend/src/access/access.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AccessController } from './access.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { AccessService } from './access.service';
+import { AccessController } from './access.controller';
+import { UserModule } from '../user/user.module';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '60m' },
+    }),
+  ],
+  providers: [AccessService, JwtStrategy],
   controllers: [AccessController],
-  providers: [AccessService]
 })
 export class AccessModule {}
--- packages/backend/src/access/access.service.ts
+++ packages/backend/src/access/access.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from '../user/user.service';
 
 @Injectable()
-export class AccessService {}
+export class AccessService {
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(email: string, password: string): Promise<any> {
+    const user = await this.userService.signIn(email, password);
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+    return user;
+  }
+
+  async login(user: any) {
+    const payload = { email: user.email, sub: user._id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
