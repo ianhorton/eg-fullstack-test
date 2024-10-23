a8ac5bcf9ebd7d6cf6d71cdaaefc933e4c4838a5
ontickdev <67747011+ontickdev@users.noreply.github.com>
2024-10-23 09:09:05 +0200

    initial commit

---
+++ packages/backend/src/config/configuration.ts
@@ -0,0 +1,7 @@
+export default () => ({
+    PORT: 5001,
+    // database: {
+    //   host: process.env.DATABASE_HOST,
+    //   port: parseInt(process.env.DATABASE_PORT, 10) || 5432
+    // }
+  });
---
+++ packages/frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('/api/hello')
+      .then(response => response.text())
+      .then(message => setMessage(message));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>{message}</h1>
+    </div>
+  );
+}
+
+export default App;
+
