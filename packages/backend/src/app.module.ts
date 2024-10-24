b0363181110bef08edb66edbe42c22a9efc0fa21
ontickdev <67747011+ontickdev@users.noreply.github.com>
2024-10-24 07:04:45 +0200

    remove access and user modules and the cofig for now

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,17 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AccessModule } from './access/access.module';
-import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-    //ConfigModule.forRoot(),
-    MongooseModule.forRoot("mongodb://localhost:/"),
-    //AccessModule,
-    //UserModule,
+    MongooseModule.forRoot("mongodb://localhost:27017/eg-fullstack-test"),
   ],
   controllers: [AppController],
   providers: [AppService],
--- packages/backend/src/config/configuration.ts
+++
@@ -1,7 +0,0 @@
-export default () => ({
-    PORT: 5001,
-    // database: {
-    //   host: process.env.DATABASE_HOST,
-    //   port: parseInt(process.env.DATABASE_PORT, 10) || 5432
-    // }
-  });
