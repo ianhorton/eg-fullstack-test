16ab1239aff732a43872f839606b56113675d5e4
ontickdev <67747011+ontickdev@users.noreply.github.com>
2024-10-25 07:09:00 +0200

    add tailwind and flowbite

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,20 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import { Button } from "flowbite-react";
 
-function App() {
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    fetch('/api/hello')
-      .then(response => response.text())
-      .then(message => setMessage(message));
-  }, []);
-
-  return (
-    <div className="App">
-      <h1>{message}</h1>
-    </div>
-  );
+export default function App() {
+  return <Button>Click me</Button>;
 }
 
-export default App;
+// import React, { useEffect, useState } from 'react';
+
+// function App() {
+//   const [message, setMessage] = useState('');
+
+//   useEffect(() => {
+//     fetch('/api/hello')
+//       .then(response => response.text())
+//       .then(message => setMessage(message));
+//   }, []);
+
+//   return (
+//     <div className="App">
+//       <h1>{message}</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
 
