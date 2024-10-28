5005a2de2b67804c9e71b56c60680a1785248e85
ontickdev <67747011+ontickdev@users.noreply.github.com>
2024-10-28 23:57:19 +0100

    docker version working

--- packages/frontend/src/api/axios.config.ts
+++ packages/frontend/src/api/axios.config.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL, // Use the environment variable here
+ // baseURL: process.env.REACT_APP_API_URL, // Use the environment variable here
+  baseURL: "http://localhost:3000"
 });
 
 export default api;
